72db405a2a97a783458f4410d702a5867d79f070
Zexin <zxuu0075@student.monash.edu>
2024-08-20 13:21:42 +1000

    Complete the week 5 task.

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,23 @@
 <script setup>
 import JSONLab from './components/JSONLab.vue'
 import BHeader from './components/BHeader.vue'
-import LibraryRegistrationForm from './components/LibraryRegistrationForm.vue'
+// import LibraryRegistrationForm from './components/LibraryRegistrationForm.vue'
+// import HomeView from './views/HomeView.vue'
+
 </script>
 
 <template>
+  <div class="main-container">
   <header>
     <BHeader />
   </header>
 
-  <main>
-    <LibraryRegistrationForm />
+  <main class="main-box">
+    <!-- <LibraryRegistrationForm /> -->
     <!-- <JSONLab /> -->
+    <router-view></router-view>
   </main>
+  </div>
 </template>
 
 <style scoped>
