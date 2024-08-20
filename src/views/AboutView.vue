27f98d1e417cf24741b31db2b2884ac059cd087a
Zexin <zxuu0075@student.monash.edu>
2024-08-20 13:29:53 +1000

    complete task 5

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -12,12 +12,4 @@
   </script>
   
   <style>
-  .container {
-  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
-  max-width: 80vw;
-  margin: 0 auto;
-  padding: 20px;
-  /* background-color: #e0bfbf; */
-  border-radius: 10px;
-}
   </style>
