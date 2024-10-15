158eaf6c38b39c7d93cdeeff7f40628dbac694a1
Zexin <zxuu0075@student.monash.edu>
2024-10-15 13:49:01 +1100

    debug

--- src/router/index.js
+++ src/router/index.js
@@ -14,7 +14,7 @@ import CountBookAPI from '../views/CountBookAPI.vue'
 import GetAllBookAPI from '../views/GetAllBookAPI.vue'
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+  history: createWebHistory(import.meta.env.VUE_APP_BASE_URL),
   routes: [
     {
       path: '/',
